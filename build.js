a9c65bac958771131d1facb6a39763710b941bd2
George Dvorak <george.dvorak@educanet.cz>
2019-12-09 00:24:43 +0100

    added comments

--- build.js
+++ build.js
@@ -14,68 +14,101 @@ export class Build {
 
 
     }
+    //function check if numberOfCards is ok
 
     checkNumberOfCards(numberOfCards) {
         
+        //checks if numberOfCards is even
         if (numberOfCards % 2 != 0) {
-
+            
+            //creates variable closetsNum which equals to numberOfCards + 1
             let closestNum = numberOfCards + 1;
+
+            //set numberOfCards to closest even number
             this.numberOfCards = closestNum;
             console.log("Pocet karet musi byt sudy. Pocet karet na staven na " + closestNum);
 
 
         }
+
+        //checks if numberOfCards is smaller than 10
         if (numberOfCards > 10) {
             this.numberOfCards = 10;
             console.log("Maximalní poèet karet je 10. Poèet karet nastaven na 10");
         }
     }
 
+    //sends the value of numberOfCards to logic.js
     sendNumberOfCardsToLogic() {
         this.logic.getNumberOfCards(this.numberOfCards);
     }
 
-
+    //this function draws the cards
     draw() {
+
+        //calls function checkNumberOfCards
         this.checkNumberOfCards(this.numberOfCards);
+        
+        //calls function sendNumberOfCardsToLogic
         this.sendNumberOfCardsToLogic();
+
+        //creates variable field which stores element with id field
         let field = document.getElementById("field");
 
-        console.log(this.numberOfCards);
 
        
-
+        //creates cards
         for (let index = 0; index < this.numberOfCards; index++) {
+
+            //create element div and stores it into a variable called card
             let card = document.createElement("div");
+
+            //set className of a card to "card"
             card.className = "card";
 
-
+            //adds card to div called "field"
             this.field.appendChild(card);
+
+            //adds card to array called "pole"
             this.pole.push(card);
             card.addEventListener("click", () => {
+
+                //calls function select from class logic and passes chosen card
                 this.logic.select(this.pole[index]);
             });
-            console.log(this.pole[index]);
+           
         }
 
     }
 
-
+    //set symbols in cards
     setSymbols() {
+
+        //creates variable called symbol
         let symbol;
 
         
-
+        //creates a loop which works until i reaches value of numberOfCards devided by 2
         for (let i = 0; i < this.numberOfCards / 2; i++) {
+
+            //chooses symbol from pole symbol on position -> i <-
             symbol = this.poleSymbol[i];
             
             for (let x = 0; x < 2; x++) {
                 
-
+                //creates div element and stores it into a variable called symbolElement
                 let symbolElement = document.createElement("div");
+
+                //set className of symbolElement to "symbol"
                 symbolElement.className = "symbol";
+
+                //set id of symbolElement to "symbolPair"+ value of i
                 symbolElement.id = "symbolPair" + i;
+
+                //set inner text of symbolElement to chosen symbol from above
                 symbolElement.innerText = symbol;
+
+                //calls function getRandomCard and add symbol to it
                 this._getRanCard().appendChild(symbolElement);
                 
             }
@@ -84,7 +117,7 @@ export class Build {
 
     }
 
-
+    //returns random card
     _getRanCard() {
 
         return this.pole[this._getRanNum()];
@@ -112,6 +145,8 @@ export class Build {
 
 
     _isNumUsed(num) {
+        //logical operation -> if usedNumber equal passed atribute than returns true
+        //else returns false
         if (this.usedNumbers.includes(num)) {
             return true;
         }
--- logic.js
+++ logic.js
@@ -4,62 +4,86 @@ export class Logic {
     //if (document.getElementById('divId').innerHTML.indexOf("word") != -1) { }
 
     constructor() {
+        //cards left to pick
         this.cardsLeft = 2;
+
+        //picked cards
         this.cardsPicked = [];
-     
+
+        //symbols inside picked cards
         this.cardsPickedSymbols = [];
+
+        //array of cards which where found
         this.cardFound = [];
+
+        //decleration of variable number of cards
         this.numberOfCards = 0;
 
 
 
     }
 
+    //Returns number of cards
     getNumberOfCards(numOfCards) {
-      this.numberOfCards = numOfCards;
+        this.numberOfCards = numOfCards;
     }
+
+    //Check if there is no cards left.
     checkWin() {
-       
-        setTimeout(()=>{
+
+        setTimeout(() => {
             console.log(this.numberOfCards);
             console.log(this.cardFound.length);
-            
+
             if (this.cardFound.length == this.numberOfCards) {
                 alert("YOU WON");
                 location.reload();
             } else {
-               
+
             }
-        },500);
-        
-       
+        }, 500);
+
+
     }
 
     //select cards
-
     select(card) {
 
-
+        //Check if firt card hasn't been chosen.
         if (this.cardsPicked[0] == undefined) {
-            
+
 
             this.cardsLeft--;
+            //Add picked card to array
             this.cardsPicked.push(card);
-            this.cardsPicked[0].className="card selected";
-            this.cardsPicked[0].getElementsByClassName("symbol").item(0).style="visibility: visible";
+
+            //Set class of the first card to "selected"
+            this.cardsPicked[0].className = "card selected";
+
+            //Set visibility of the first symbol to "visibile"
+            this.cardsPicked[0].getElementsByClassName("symbol").item(0).style = "visibility: visible";
 
         }
 
-
+        //Check if second card hasn't been chosen.
         else if (this.cardsPicked[1] == undefined) {
 
+            //Check if second card doesn't equal to first card
             if (card != this.cardsPicked[0]) {
+
+                //Add picked card to array
                 this.cardsPicked.push(card);
-                this.cardsPicked[1].className="card selected";
+
+                //Set class of the first card to "selected"
+                this.cardsPicked[1].className = "card selected";
+
                 this.cardsLeft--;
-                this.cardsPicked[1].getElementsByClassName("symbol").item(0).style="visibility: visible";
+
+                //Set visibility of the first symbol to "visibile"
+                this.cardsPicked[1].getElementsByClassName("symbol").item(0).style = "visibility: visible";
 
             } else {
+                //output "invalid move" if code above returns false
                 console.log("invalid move");
             }
         }
@@ -67,7 +91,7 @@ export class Logic {
 
 
 
-
+        //Calls function getSelectedCardsSymbol() if player selected 2 cards
         if (this.cardsPicked.length == 2) {
             this.getSelectedCardsSymbols(this.cardsPicked[0], this.cardsPicked[1]);
         }
@@ -78,57 +102,78 @@ export class Logic {
     //get symbols from selected cards
 
     getSelectedCardsSymbols(card1, card2) {
+
+        //select symbol from card1
         let symbol1 = card1.getElementsByClassName("symbol").item(0);
+
+        //select symbol from card2
         let symbol2 = card2.getElementsByClassName("symbol").item(0);
 
 
 
-
+        //calls function checkSymbols()
         this._checkSymbols(symbol1, symbol2)
 
 
     }
 
-
+    //This function compare 2 symbols of chosen cards
     _checkSymbols(cardSym1, cardSym2) {
+
+        //check if chosen symbols match
         if (cardSym1.innerText == cardSym2.innerText) {
-            this.cardFound.push("x", "c");
-            setTimeout(()=>{
+            //if true -> push 2 cards into cardFound array
+            this.cardFound.push(this.cardsPicked[0], this.cardsPicked[1]);
+
+            //set timeOut to 600 ms
+
+            setTimeout(() => {
+                //set visibility of the first card to hidden
                 this.cardsPicked[0].style = "visibility: hidden";
+
+                //set visibility of the second card to hidden
                 this.cardsPicked[1].style = "visibility: hidden";
-            },600);
-          
-           
-            
-            setTimeout(()=>{
+            }, 600);
+
+
+            //set timeOut to 600 ms
+            setTimeout(() => {
+
+                //reset cardsPicked array
                 this.cardsPicked.length = 0;
+
+                //calls function checkWin
                 this.checkWin();
 
-            },600);
-         
-        } else {
-            setTimeout(()=>{
-                this.cardsPicked[1].getElementsByClassName("symbol").item(0).style="visibility: hidden";
-                this.cardsPicked[0].getElementsByClassName("symbol").item(0).style="visibility: hidden";
-                this.cardsPicked[0].className="card";
-                this.cardsPicked[1].className="card";
-            },600);
-            
-            setTimeout(()=>{
-                this.cardsPicked.length = 0;
-               
+            }, 600);
 
-            },600);
+        } else {
+            //set timeOut to 600 ms
+            setTimeout(() => {
+                //set visibility of the second card to hidden
+                this.cardsPicked[1].getElementsByClassName("symbol").item(0).style = "visibility: hidden";
+                //set visibility of the first card to hidden
+                this.cardsPicked[0].getElementsByClassName("symbol").item(0).style = "visibility: hidden";
+
+
+                //set className of first card to "card"
+                this.cardsPicked[0].className = "card";
+                //set className of second card to "card"
+                this.cardsPicked[1].className = "card";
+            }, 600);
+
+            //set timeOut to 600 ms
+            setTimeout(() => {
+
+                //reset cardsPicked array
+                this.cardsPicked.length = 0;
+
+
+            }, 600);
         }
     }
 
 
-
-
-
-    showCard() {
-
-    }
 
 }
 
